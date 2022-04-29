ad565c9db49ec714a3b1e4a049d16d7ac8dc721a
yujinj <yujinii0627@gmail.com>
2022-04-28 23:01:45 -0700

    stuff

--- R/distributional_uncertainty.R
+++ R/distributional_uncertainty.R
@@ -7,7 +7,7 @@
 #' # Compare to standard normal
 #' qqnorm(rnorm(1000))
 #' @export
-distributional_seed <- function(n,delta){
+distributional_seed <- function(n, delta){
   ## Set distributional seed
   m <- round(n/(delta^2-1),0)
   #Exact choice of delta is often not possible.
@@ -49,12 +49,36 @@ distributional_seed <- function(n,delta){
 #' n*vec[5]/var(rnorm(10000)^2)
 #' n*vec[6]/var(rnorm(10000)*rnorm(10000))
 #' @export
-drnorm <- function(d_seed,mean=0,sd=1){
+drnorm <- function(d_seed, mean = 0, sd = 1){
   unique <- rnorm(max(d_seed))
   rv <- unique[d_seed] + 1/sqrt(length(unique(d_seed)))*rnorm(length(d_seed))
   return(mean + rv*sd)
 }
 
+#' Distributional random number generation (equivalent to runif)
+#' @param d_seed A distributional seed as returned by distributional_seed()
+#' @param min lower limit of the unperturbed uniform distribution
+#' @param max upper limit of the unperturbed uniform distribution
+#' @examples
+#' d_seed <- distributional_seed(1000,10)
+#' drunif(d_seed)
+drunif <- function(d_seed, min = 0, max = 1){
+  unique <- runif(max(d_seed))
+  rv <- unique[d_seed] + 1/sqrt(length(unique(d_seed)))*runif(length(d_seed))
+  return(min + (max-min)*rv)
+}
+
+#' Distributional random number generation (equivalent to rbinom)
+#' @param d_seed A distributional seed as returned by distributional_seed()
+#' @param size number of trials 
+#' @param prob probability of success on each trial
+#' @examples
+#' d_seed <- distributional_seed(1000,10)
+#' drbinom(d_seed)
+drbinom <- function(d_seed, size, prob){
+  unique <- rbinom(max(d_seed), size, prob)
+  return(unique[d_seed])
+}
 
 #' Calibrated inference for linear models
 #' @param formulas A list of formulas
@@ -69,7 +93,7 @@ drnorm <- function(d_seed,mean=0,sd=1){
 #' data <- as.data.frame(cbind(Y,X))
 #' formulas <- list(Y~X, Y ~ X + I(X^2), Y ~ X + Z1, Y ~ X + Z2 + I(X^2), Y ~ X + Z1 + Z2 + I(X^2))
 #' calm(formulas,data=data, target="X")
-#' summary(lm(Y~X + Z1 + Z2 + X^2,data=df))
+#' summary(lm(Y ~ X + Z1 + Z2 + I(X^2), data=df))
 #' @export
 calm <- function( formulas, data, target, ...){
 
@@ -79,9 +103,8 @@ calm <- function( formulas, data, target, ...){
 
   for (j  in 1:length(formulas)){
     model <- lm(formulas[[j]],data=data)
-    # Remark: the influence() function also exists for glms! We should be able to create a calibratedglm function analogously
     infls[[j]] <- n*influence(model)$coefficients[,target]
-    coefs[[j]] <-  coef(model)[target]
+    coefs[[j]] <- coef(model)[target]
   }
 
   infls <- simplify2array(infls)
@@ -90,7 +113,7 @@ calm <- function( formulas, data, target, ...){
   # Careful! I haven't tested the coverage of this extensively yet! This is just a prototype.
   estimate <- mean(coefs)
   sd_estimate <- sd(rowMeans(infls))
-  delta <-   sqrt(n)*sqrt(mean( (coefs - mean(coefs))^2/ apply( infls - rowMeans(infls),2,var)))
+  delta <- sqrt(n)*sqrt(mean( (coefs - mean(coefs))^2/ apply( infls - rowMeans(infls),2,var)))
 
   p_values <- 2*pnorm(abs(estimate)/(sd_estimate*delta/sqrt(n)),lower.tail=FALSE)
 
@@ -106,7 +129,65 @@ calm <- function( formulas, data, target, ...){
   cat("hat delta = ")
   cat(delta)
   cat("\n")
+  
+  #return(delta)
+  invisible(ret_table)
 
 }
 
+#' Calibrated inference for generalized linear models
+#' @param formulas A list of formulas
+#' @param family a description of the error distribution and link function to be used in the model
+#' @param data A dataframe containing the variables in the model
+#' @examples
+#' n <- 1000
+#' X <- rnorm(n)
+#' Z1 <- rnorm(n)
+#' Z2 <- rnorm(n)
+#' logit <- 1*X + X^2 + Z1 + Z2
+#' Y = rbinom(n, size = 1, prob = exp(logit)/(1+exp(logit)))
+#' df <- data.frame(cbind(X,Y,Z1,Z2))
+#' data <- as.data.frame(cbind(Y,X))
+#' formulas <- list(Y~X, Y ~ X + I(X^2), Y ~ X + Z1, Y ~ X + Z2 + I(X^2), Y ~ X + Z1 + Z2 + I(X^2))
+#' caglm(formulas, family = "binomial", data=data, target="X")
+#' summary(glm(Y ~ X + Z1 + Z2 + I(X^2), family = "binomial", data=df))
+#' @export
+caglm <- function(formulas, family, data, target, ...){
+  
+  n <- nrow(data)
+  infls <- vector(mode = "list", length = length(formulas))
+  coefs <-  vector(mode = "list", length = length(formulas))
+  
+  for (j  in 1:length(formulas)){
+    model <- glm(formulas[[j]], family = family, data=data)
+    print(model)
+    infls[[j]] <- n*influence(model)$coefficients[,target]
+    coefs[[j]] <- coef(model)[target]
+  }
+  
+  infls <- simplify2array(infls)
+  coefs <- simplify2array(coefs)
+  
+  # Careful! I haven't tested the coverage of this extensively yet! This is just a prototype.
+  estimate <- mean(coefs)
+  sd_estimate <- sd(rowMeans(infls))
+  delta <- sqrt(n)*sqrt(mean( (coefs - mean(coefs))^2/ apply( infls - rowMeans(infls),2,var)))
+  
+  p_values <- 2*pnorm(abs(estimate)/(sd_estimate*delta/sqrt(n)),lower.tail=FALSE)
+  
+  ret_table <- cbind(estimate,sd_estimate*delta/sqrt(n),p_values)
+  ret_table <- round(ret_table,4)
+  colnames(ret_table) <- c("Estimate", "Std. Error", "Pr(>|z|)")
+  rownames(ret_table) <- target
+  cat("\n")
+  cat("Quantification of both distributional and sampling uncertainty")
+  cat("\n\n")
+  print(ret_table)
+  cat("\n")
+  cat("hat delta = ")
+  cat(delta)
+  cat("\n")
+  
+  invisible(ret_table)
+}
 
